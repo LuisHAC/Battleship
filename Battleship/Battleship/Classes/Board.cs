080e485271b30e9732ea3f2c6d3c92bb232a86b0
Luis Avendano <luisavendanocastanos@hotmail.com>
2018-10-29 12:48:19 -0700

    Changes

---
+++ Battleship/Battleship/Classes/Board.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship.Classes
+{
+    class Board
+    {
+
+        private Byte[,] matrix;
+        private char type;
+        private char[] status;
+        //Type puede ser 'O' para el tablero de ofensiva o 'D' para el defensivo (donde se ponen los barcos)
+        public Byte Value(int row, int column)
+        {
+            return matrix[row, column];
+        }
+        public char Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        public char[] Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+        public Board()
+        {
+            matrix = new byte[10, 10];
+            type = 'D';
+            status = new char[5];
+        }
+
+    }
+}
---
+++ Battleship/Battleship/Classes/Ship.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship.Classes
+{
+    class Ship
+    {
+        #region Variables
+        private int size;
+        private int x;
+        private int y;
+        private bool direction;
+        #endregion
+        #region Get/Sets
+        public int Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+        public int X
+        {
+            get { return x; }
+            set { size = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { size = value; }
+        }
+        public bool Direction
+        {
+            get { return direction; }
+            set { direction = value; }
+        }
+        #endregion
+
+
+    }
+}
