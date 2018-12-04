058202414c9bb37c0bc37343ee59b8b857ddc065
Gilberto Garcia <estebario_@hotmail.com>
2018-12-03 18:16:18 -0800

    Cambios en el GUI y lógica de juego, no esta completado

---
+++ Battleship/Classes/Globals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship.Classes
+{
+    class Globals
+    {
+        public static Board Offense = new Board(false);
+        public static Board Defense = new Board(true);
+        public static Board Map = new Board(false);
+         
+    }
+}
---
+++ Battleship/Classes/Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship.Classes
+{
+    class Point
+    {
+        private int x;
+        private int y;
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public Point()
+        {
+            x = 0;
+            y = 0;
+        }
+        public Point(int a, int b)
+        {
+            x = a;
+            y = b;
+        }
+    }
+}
